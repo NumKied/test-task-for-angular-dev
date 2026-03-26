df8585a8eaa4a5d114e2f68a7da8612dd86e673c
NumKied <nazarkrabik@gmail.com>
2026-03-26 02:26:06 +0200

    finished migration to nx

---
+++ apps/ng-x-rocket/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { environment } from '@ng-x-rocket/env/environment';
+import { CoreModule } from '@ng-x-rocket/core';
+import { SharedModule } from '@ng-x-rocket/shared';
+import { AuthModule } from '@ng-x-rocket/auth';
+import { HomeModule } from './home/home.module';
+import { ShellModule } from './shell/shell.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production }),
+    FormsModule,
+    TranslateModule.forRoot(),
+    NgbModule,
+    CoreModule,
+    SharedModule,
+    ShellModule,
+    HomeModule,
+    AuthModule,
+    AppRoutingModule,
+  ],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
+})
+export class AppModule {}
