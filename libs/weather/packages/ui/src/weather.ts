ff4185ceef45a3d5af590bf80013553f7b43879a
NumKied <nazarkrabik@gmail.com>
2026-03-27 21:09:18 +0200

    generated 'weather' library

---
+++ libs/weather/packages/ui/src/weather.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-weather',
+  templateUrl: './weather.html',
+  styleUrl: './weather.scss',
+  imports: [],
+})
+export class Weather {}
