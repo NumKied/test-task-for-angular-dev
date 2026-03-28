d5bc2f90869908340bb0242a9f2dfe6a9a58a747
NumKied <nazarkrabik@gmail.com>
2026-03-28 04:58:07 +0200

    created 'weather' library

--- apps/ng-x-rocket/src/app/home/home.module.ts
+++ apps/ng-x-rocket/src/app/home/home.module.ts
@@ -5,9 +5,10 @@ import { TranslateModule } from '@ngx-translate/core';
 import { SharedModule } from '@ng-x-rocket/shared';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { Weather } from 'weather';
 
 @NgModule({
-  imports: [CommonModule, TranslateModule, SharedModule, HomeRoutingModule],
+  imports: [CommonModule, TranslateModule, SharedModule, HomeRoutingModule, Weather],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
--- libs/weather/packages/ui/src/weather.ts
+++ libs/weather/packages/ui/src/weather.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+
+import { BlockBuilder } from 'block-builder';
+import { WeatherState } from '../../state';
+
+@UntilDestroy()
 @Component({
   selector: 'lib-weather',
   templateUrl: './weather.html',
   styleUrl: './weather.scss',
-  imports: [],
+  imports: [BlockBuilder, MatButtonModule],
 })
-export class Weather {}
+export class Weather {
+  private state = inject(WeatherState);
+
+  public weatherState = computed(() => (Object.keys(this.state.state()).length ? this.state.state() : null));
+
+  public getWeather(): void {
+    this.state.getWeather().pipe(untilDestroyed(this)).subscribe();
+  }
+}
